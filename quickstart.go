d828978c60e4a3e047ac113043d1ce2e4f64a61d
zaramichaela <46987186+zaramichaela@users.noreply.github.com>
2023-03-05 22:04:36 +0800

    Update main

--- quickstart.go
+++ quickstart.go
@@ -11,3 +11,32 @@ import (
 	"google.golang.org/api/gmail/v1"
 	"google.golang.org/api/option"
 )
+
+func main() {
+	ctx := context.Background()
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, gmail.MailGoogleComScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	srv, err := gmail.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Gmail client: %v", err)
+	}
+
+	user := "me"
+
+	c := cron.New()
+	c.AddFunc("0 3 * * * *", func() sendmail(*srv)})
+	c.Start()
+	for {
+	}
+
+}
